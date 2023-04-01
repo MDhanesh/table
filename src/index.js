7353783e879d2d57eac2e5de6b3208d264b3a531
MDhanesh <mgdhanesh98@gmail.com>
2023-04-01 16:29:08 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+///
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
--- src/table2.jsx
+++ src/table2.jsx
@@ -1,71 +1,111 @@
-import React from "react";
-import DataTable from "react-data-table-component";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button } from "primereact/button";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import { Link } from "react-router-dom";
+import { FilterMatchMode } from "primereact/api";
+import { InputText } from "primereact/inputtext";
 
 const TableSecond = () => {
-  const columns = [
-    {
-      name: "Name",
-      selector: (row) => row.name,
-      sortable: true,
-    },
-    {
-      name: "Age",
-      selector: (row) => row.age,
-      sortable: true,
-    },
-    {
-      name: "Email",
-      selector: (row) => row.email,
-      sortable: true,
-    },
-  ];
-  const data = [
-    {
-      id: "1",
-      name: "ajay",
-      age: "22",
-      email: "123@gmail.com",
-    },
-    {
-      id: "2",
-      name: "next",
-      age: "72",
-      email: "necy3@gmail.com",
-    },
-    {
-      id: "3",
-      name: "abis",
-      age: "42",
-      email: "abis@gmail.com",
-    },
-    {
-      id: "4",
-      name: "ajaywin",
-      age: "25",
-      email: "ajay@gmail.com",
-    },
-    {
-      id: "5",
-      name: "winzen",
-      age: "25",
-      email: "winzen@gmail.com",
-    },
-    {
-      id: "6",
-      name: "anto",
-      age: "35",
-      email: "anto@gmail.com",
-    },
-  ];
-  return (
-    <div className="container mt-5">
-      <h1>TABLE SECOND</h1>
-      <button>
-        <Link to="/">NEXT SCREEN</Link>
-      </button>
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
 
-      <DataTable columns={columns} data={data} pagination></DataTable>
+  const paginatorLeft = (
+    <Button
+      type="button"
+      icon="pi pi-refresh"
+      text
+      onClick={() => window.location.reload()}
+    />
+  );
+  //   const paginatorRight = <Button type="button" icon="pi pi-download" text />;
+
+  //
+  useEffect(() => {
+    async function getleads() {
+      const response = await axios.get(
+        `https://62ff2d03a85c52ee48420f24.mockapi.io/Data`
+      );
+      console.log(response.data);
+      setData(response.data);
+    }
+
+    getleads();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h1 className="d-flex mt-4 justify-content-center">Second Screen</h1>
+      </div>
+      <div className="d-flex mt-4 justify-content-center">
+        <Link to="/">
+          <button className="btn btn-success">FIRST SCREEN</button>
+        </Link>
+      </div>
+
+      <div className="d-flex my-4 justify-content-center ">
+        <InputText
+          className="bg-black text-white"
+          placeholder="search name..."
+          onInput={(e) =>
+            setSearchTerm({
+              global: {
+                value: e.target.value,
+                matchMode: FilterMatchMode.CONTAINS,
+              },
+            })
+          }
+        />
+      </div>
+      <div className="card">
+        <DataTable
+          value={data}
+          sortMode="multiple"
+          paginator
+          filters={searchTerm}
+          rows={4}
+          rowsPerPageOptions={[4, 8, 12, 18, 24, 30]}
+          tableStyle={{ minWidth: "50rem" }}
+          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+          currentPageReportTemplate="{first} to {last} of {totalRecords}"
+          paginatorLeft={paginatorLeft}
+        >
+          <Column
+            field="id"
+            header="ID"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="name"
+            header="Name"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="age"
+            header="Age"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="email"
+            header="Email"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="course"
+            header="Course"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+        </DataTable>
+      </div>
     </div>
   );
 };
